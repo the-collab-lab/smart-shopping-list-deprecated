4188ece9cef10d779a236f326082c54d4feed9bf
Stacie Taylor-Cima <stacietaylorcima@gmail.com>
2019-11-03 10:09:03 -0800

    Add firebase.js template
    We've added a file where all the firebase env vars will be udated and stored when a new firebase project is created for each new cohort.

---
+++ src/lib/firebase.js
@@ -0,0 +1,20 @@
+// NOTE: import only the Firebase modules that you need in your app... except
+// for the second line, which makes both the linter and react-firebase happy
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+// Initalize Firebase.
+// These details will need to be replaced with the project specific env vars at the start of each new cohort.
+var firebaseConfig = {
+    apiKey: "api-key",
+    authDomain: "project-id.firebaseapp.com",
+    databaseURL: "https://project-id.firebaseio.com",
+    projectId: "project-id",
+    storageBucket: "project-id.appspot.com",
+    messagingSenderId: "sender-id",
+    appId: "app-id",
+};
+
+let fb = firebase.initializeApp(config);
+
+export {fb};
