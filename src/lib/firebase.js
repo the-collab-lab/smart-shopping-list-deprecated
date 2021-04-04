8e2a837ec286b2e3a4d1c175ce9e4249f1a531c9
Lauren Beatty <lauren.beatty@treasure-data.com>
2021-04-03 20:30:29 -0500

    add firestore

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,13 +6,13 @@ import 'firebase/firestore';
 // Initalize Firebase.
 // These details will need to be replaced with the project specific env vars at the start of each new cohort.
 var firebaseConfig = {
-  apiKey: { TCL_X_API_KEY },
-  authDomain: { TCL_X_AUTH_DOMAIN },
-  databaseURL: { TCL_X_DATABASE_URL },
-  projectId: { TCL_X_PROJECT_ID },
-  storageBucket: { TCL_X_STORAGE_BUCKET },
-  messagingSenderId: { TCL_X_SENDER_ID },
-  appId: { TCL_X_APP_ID },
+  apiKey: "AIzaSyAyo4_edWgd_ytXNkwowxxb8oVqsWN4YJA",
+  authDomain: "blog-88fa0.firebaseapp.com",
+  databaseURL: "https://blog-88fa0.firebaseio.com",
+  projectId: "blog-88fa0",
+  storageBucket: "blog-88fa0.appspot.com",
+  messagingSenderId: "118449606222",
+  appId: "1:118449606222:web:5de10609ce43c168763d1f"
 };
 
 let fb = firebase.initializeApp(firebaseConfig);
