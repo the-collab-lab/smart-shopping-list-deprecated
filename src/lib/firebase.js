a678a00077adb9748c87037e4b829fdd3b9b86b5
Lauren Beatty <lauren.beatty@treasure-data.com>
2021-04-04 20:57:58 -0500

    try this

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,13 +6,13 @@ import 'firebase/firestore';
 // Initalize Firebase.
 // These details will need to be replaced with the project specific env vars at the start of each new cohort.
 var firebaseConfig = {
-  apiKey: "AIzaSyAyo4_edWgd_ytXNkwowxxb8oVqsWN4YJA",
-  authDomain: "blog-88fa0.firebaseapp.com",
-  databaseURL: "https://blog-88fa0.firebaseio.com",
-  projectId: "blog-88fa0",
-  storageBucket: "blog-88fa0.appspot.com",
-  messagingSenderId: "118449606222",
-  appId: "1:118449606222:web:5de10609ce43c168763d1f"
+  apiKey: "AIzaSyDZVtNP8l1z8u36In4tJZrOdIOn2aNCwGo",
+  authDomain: "shopping-c16d8.firebaseapp.com",
+  projectId: "shopping-c16d8",
+  storageBucket: "shopping-c16d8.appspot.com",
+  messagingSenderId: "979351683661",
+  appId: "1:979351683661:web:ef378e0975539b981c1204",
+  measurementId: "G-J69XJ99QHT"
 };
 
 let fb = firebase.initializeApp(firebaseConfig);
